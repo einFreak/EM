f82c59688caf452f64fed4485f0dc5eb5926ab86
Felix <32998993+einFreak@users.noreply.github.com>
2020-02-05 10:42:32 +0100

    Add files via upload

---
+++ 7. XMC LED/Debug/Startup/system_XMC4500.d
@@ -0,0 +1,27 @@
+Startup/system_XMC4500.o \
+ Startup/system_XMC4500.d Startup/system_XMC4500.o: \
+ ../Startup/system_XMC4500.c \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/XMC4500.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/core_cm4.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_version.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/mpu_armv7.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h \
+ C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/XMC4500.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/core_cm4.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_version.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h:
+
+C:/Users/wif32384/Desktop/7.\ XMC\ Einf/XMC-LED/Libraries/CMSIS/Infineon/XMC4500_series/Include/system_XMC4500.h:
